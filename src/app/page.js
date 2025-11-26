e10eed5461ae8ea834b99ea49fb5533863d50694
Easin-Dev <mdeasinarafat016456@gmail.com>
2025-11-26 15:48:14 +0600

    some error show and error fix

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,9 @@
-import Footer from "@/components/Footer";
 import HeroSection from "@/components/HeroSection";
-import Navbar from "@/components/navbar";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-     
       <HeroSection />
-     
     </div>
   );
 }
